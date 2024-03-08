66654d79aea0bb9b0fe48dd7a03b6e844ffb35e0
Midora Shiu <midorashiu@gmail.com>
2024-03-07 18:08:52 -0800

    added test code for motor with encoder

--- test_code/motor_encoder/motor_encoder.ino
+++ test_code/motor_encoder/motor_encoder.ino
@@ -13,31 +13,38 @@ int _fastIncrement = 1;
 
 volatile int counter = 0;
 
+// PID constants
+float kp = 1;
+float kd = 0.025;
+float ki = 0.0;
+
 //vars for PID
-volatile int posi = 0; 
+int pos = 0; 
 long prevT = 0;
 float eprev = 0;
 float eintegral = 0;
 
-enum{HOME, FORWARD, BACKWARDS};
-int state;
+int target_positions = [8, 16, 8, 0];
 int target;
+int n = 0;
 
 void setup() {
-
+  // Start the serial monitor to show output
+  Serial.begin(115200);
   // Set encoder pins and attach interrupts
   pinMode(ENC_A, INPUT_PULLUP);
   pinMode(ENC_B, INPUT_PULLUP);
   pinMode(PWMA,OUTPUT);
   pinMode(PWMB,OUTPUT);
+  
   attachInterrupt(digitalPinToInterrupt(ENC_A), read_encoder, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENC_B), read_encoder, CHANGE);
-
-  // Start the serial monitor to show output
-  Serial.begin(115200);
+  attachInterrupt(digitalPinToInterrupt(ENC_B), read_encoder, CHANGE); 
+  
+  Serial.println("target pos");
 }
 
 void loop() {
+
   static int lastCounter = 0;
 
   // If count has changed print the new value to serial
@@ -45,8 +52,55 @@ void loop() {
     Serial.println(counter);
     lastCounter = counter;
   }
+
+  //set target position
+  target = target_positions[n];
+  if (pos==target){ //once we reach a position, go to next position
+    if (n<4) n++;
+    else n=0;
+  }
+
+  // time difference
+  long currT = micros();
+  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
+  prevT = currT;
+  
+  //read position
+  pos = counter;
+
+  //PID
+  int e = pos - target; // error
+  float dedt = (e-eprev)/(deltaT); // derivative
+  eintegral = eintegral + e*deltaT; // integral
+
+  // control signal
+  float pwmVal = kp*e + kd*dedt + ki*eintegral;
+  moveMotor(pwmVal);
+  eprev = e;
+  Serial.print(target);
+  Serial.print(" ");
+  Serial.print(pos);
+  Serial.println();
 }
 
+void moveMotor(float pwmVal){
+  float motorSpeed = constrain(abs(pwmVal), 0, 255)
+  if (pwmVal>0)                    
+  {
+    analogWrite(PWMA, motorSpeed); 
+    analogWrite(PWMB, 0); 
+  }
+  else if(pwmVal<0)
+  {
+    analogWrite(PWMA, 0); 
+    analogWrite(PWMB, motorSpeed); 
+  }
+  else
+  {
+    analogWrite(PWMA, 0); 
+    analogWrite(PWMB, 0); 
+  }
+}
 void read_encoder() {
   // Encoder interrupt routine for both pins. Updates counter
   // if they are valid and have rotated a full indent
